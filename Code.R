18a85e663c680964510454e0fcbf37a3ae87aa35
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-07-06 13:19:45 +0800

    Create Code.R

---
+++ Code.R
@@ -0,0 +1,12 @@
+library(AgroR)
+
+
+qmres=c(2, 12, 25, 3, 7, 55, 2)
+jointcluster(qmres,information = "cluster")
+
+
+jointcluster(qmres,information = "matrix")
+
+
+jointcluster(qmres,information = "bar")
+
